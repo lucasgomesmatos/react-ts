d9c9d71ab32d1e3c286cb108bdc402246c464b2b
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-21 12:22:41 -0300

    theme ligth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@emotion/react';
+import { LightTheme } from './shared/themes';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
