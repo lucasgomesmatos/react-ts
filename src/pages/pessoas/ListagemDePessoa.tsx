a886c579dfbdf7957b0384ec28cfb5be5c6c8fe2
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-10-21 22:51:45 -0300

    debounce

--- src/pages/pessoas/ListagemDePessoa.tsx
+++ src/pages/pessoas/ListagemDePessoa.tsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const { debounce } = useDebounce(2000);
+
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
-  console.log(busca);
-
   useEffect(() => {
-    PessoasService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        console.log(result.message);
-        return;
-      } else {
-        console.log(result.data);
-      }
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          console.log(result.message);
+          return;
+        } else {
+          console.log(result.data);
+        }
+      });
     });
   }, [busca]);
 
--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from "./UseBebounce"
