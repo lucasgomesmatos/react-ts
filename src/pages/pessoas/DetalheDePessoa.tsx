c848460f0f1bfa5515de885a77fe2cd530e00637
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-06 10:16:38 -0300

    detalhe de pessoa

--- src/pages/pessoas/DetalheDePessoa.tsx
+++ src/pages/pessoas/DetalheDePessoa.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FerramentaDeDetalhe } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
-
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      PessoasService.getById;
+    }
+  }, [id]);
 
   return (
     <LayoutBaseDePagina
